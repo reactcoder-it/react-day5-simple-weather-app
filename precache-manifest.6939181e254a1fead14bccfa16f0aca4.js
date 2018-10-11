632d57892cea039076e618836c1ba3e510a153e8
Vadim Pashaev <vmp@live.ru>
2018-10-11 21:04:21 +0400

    Updates

---
+++ precache-manifest.6939181e254a1fead14bccfa16f0aca4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "70032a4ae44bd7ed40a1",
+    "url": "/react-day5-simple-weather-app/static/js/runtime~main.70032a4a.js"
+  },
+  {
+    "revision": "1d05ecb6205efdd060a8",
+    "url": "/react-day5-simple-weather-app/static/js/main.1d05ecb6.chunk.js"
+  },
+  {
+    "revision": "4fd91c74e51506835aa9",
+    "url": "/react-day5-simple-weather-app/static/js/1.4fd91c74.chunk.js"
+  },
+  {
+    "revision": "1d05ecb6205efdd060a8",
+    "url": "/react-day5-simple-weather-app/static/css/main.351d0e0f.chunk.css"
+  },
+  {
+    "revision": "a7190c5ecdff719844a4da48d1ee65b5",
+    "url": "/react-day5-simple-weather-app/index.html"
+  }
+];
